929e08fa2a0a3f7033228979c3cfebb3e89a8eb9
aminebenichou <aamine060@gmail.com>
2026-01-27 15:19:35 +0100

    sync

--- xo.py
+++ xo.py
@@ -13,15 +13,18 @@ small_font = pygame.font.SysFont(None, 50)
 
 class Player:
 
-    sign="x"
-    def alternate(self, is_valid_x, is_valid_o, turn):
-        if turn%2==0 and not is_valid_o:
-                self.sign="x"
-                cell['x_checked']=True 
-        elif turn%2!=0 and not is_valid_x:
-                self.sign="o"
-                cell['o_checked']=True
+    sign=None
 
+    def choose_sign(self, choice):
+        self.sign = choice
+
+    def alternate(self, is_valid_x, is_valid_o, turn):
+        if self.sign=="x" and not is_valid_o:
+            self.sign='o'
+        elif self.sign=="o" and not is_valid_x:
+            self.sign='x'
+
+            
 
     def draw(self):
         return f"{self.sign}_checked"
@@ -54,6 +57,7 @@ mouse=(0,0)
 turn = 0
 paused = False
 player=Player()
+clicks = 0
 while running:
     # print(cells)
     # poll for events
@@ -63,39 +67,67 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN and not paused:
+            clicks += 1
+            print(clicks)
             mouse=pygame.mouse.get_pos()
             print(mouse)
-            turn += 1
+            if player.sign != None:
+                turn += 1
             
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
-    for cell in cells:
-        pygame.draw.rect(screen, 'white', (cell['start_pos'][0], cell['start_pos'][1], 190, 190))
-        if cell['start_pos'][0]<mouse[0]<cell['end_pos'][0] and cell['start_pos'][1]<mouse[1]<cell['end_pos'][1]:
-            player.alternate(cell['x_checked'], cell['o_checked'], turn)
-            cell[player.draw()]=True
-            
+    if player.sign == None:
+        pygame.draw.rect(screen, 'black', (100, 200, 150, 150))
+        x= small_font.render('X', False, 'white')
+        screen.blit(x, (150, 250))
+        pygame.draw.rect(screen, 'black', (300, 200, 150, 150))
+        o= small_font.render('O', False, 'white')
+        screen.blit(o, (350, 250))
+
+
+        if 100<mouse[0]<250  and 200<mouse[1]<350:
+            player.choose_sign('x')
+            print('mouse: ', mouse)
+            mouse=(0, 0)
+
+        elif 300<mouse[0]<450 and 200<mouse[1]<350:
+            player.choose_sign('o')
+            print('mouse: ', mouse)
+            mouse=(0, 0)
+
 
         
-        if cell['x_checked']:
-            text_surface = my_font.render('X', False, 'black')
-            screen.blit(text_surface, (cell['start_pos'][0]+10, cell['start_pos'][1]))
-        if cell['o_checked']:
-            text_surface = my_font.render('O', False, 'black')
-            screen.blit(text_surface, (cell['start_pos'][0]+10, cell['start_pos'][1]))
+    else:
+        # RENDER YOUR GAME HERE
+        for cell in cells:
+            pygame.draw.rect(screen, 'white', (cell['start_pos'][0], cell['start_pos'][1], 190, 190))
+            if cell['start_pos'][0]<mouse[0]<cell['end_pos'][0] and cell['start_pos'][1]<mouse[1]<cell['end_pos'][1]:
+                print("drawing launched")
+                print(cell)
+                player.alternate(cell['x_checked'], cell['o_checked'], turn)
+                cell[player.draw()]=True
+                print(cell)
+                mouse=(0,0)
 
-    for comb in combinations:
-        if cells[comb[0]]['x_checked'] and cells[comb[1]]['x_checked'] and cells[comb[2]]['x_checked']:
-            text_surface=small_font.render('X WON', False, 'red')
-            screen.blit(text_surface, (300, 300))
-            paused=True
-        elif cells[comb[0]]['o_checked'] and cells[comb[1]]['o_checked'] and cells[comb[2]]['o_checked']:
-            text_surface=small_font.render('O WON', False, 'red')
-            screen.blit(text_surface, (300, 300))
-            paused=True
+            
+            if cell['x_checked']:
+                text_surface = my_font.render('X', False, 'black')
+                screen.blit(text_surface, (cell['start_pos'][0]+10, cell['start_pos'][1]))
+            if cell['o_checked']:
+                text_surface = my_font.render('O', False, 'black')
+                screen.blit(text_surface, (cell['start_pos'][0]+10, cell['start_pos'][1]))
+
+        for comb in combinations:
+            if cells[comb[0]]['x_checked'] and cells[comb[1]]['x_checked'] and cells[comb[2]]['x_checked']:
+                text_surface=small_font.render('X WON', False, 'red')
+                screen.blit(text_surface, (300, 300))
+                paused=True
+            elif cells[comb[0]]['o_checked'] and cells[comb[1]]['o_checked'] and cells[comb[2]]['o_checked']:
+                text_surface=small_font.render('O WON', False, 'red')
+                screen.blit(text_surface, (300, 300))
+                paused=True
 
     # flip() the display to put your work on screen
     pygame.display.flip()
